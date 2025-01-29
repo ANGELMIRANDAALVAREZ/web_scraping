96a43ebc05895cec574b62580153285790ac65c7
ANGELMIRANDAALVAREZ <133290593+ANGELMIRANDAALVAREZ@users.noreply.github.com>
2025-01-29 17:56:47 -0600

    Delete 1+1 prueba.py

--- 1+1 prueba.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-# El resto de tu código de Python va aquí
-resultado = 1 + 1
-print(resultado)
-
-# Al final de tu script, añade lo siguiente para ejecutar el atajo en un proceso separado
-subprocess.Popen(["/usr/bin/shortcuts", "run", "NotificacionPython"])
